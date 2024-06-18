351939e39512cff092a5abee9cb69975aa95a185
Kacper Lubczynski <kacperlubczynski564@gmail.com>
2024-06-18 15:24:15 +0100

    forgot to generalise number of chars in string function

--- utility/additional_function.py
+++ utility/additional_function.py
@@ -1,6 +1,6 @@
 def get_number_of_chars(string : str, char_in_string : str) -> int:
     sum = 0
     for char in string:
-        if char == " ":
+        if char == char_in_string:
             sum +=1
     return sum
